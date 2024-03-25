d7acab50f2911860fb48f1ed20dee5d24251627c
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-03-25 14:54:13 -0300

    ejercicio 4

--- index.js
+++ index.js
@@ -59,10 +59,35 @@ console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igua
 
 function ejercicio4(){
 
+  var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K','E', 'T'];
 
+let dni = document.querySelector("#dni".value);
+
+let letrauser = document
+
+if(dni > 0 && dni <99999999){
+
+let nletra = dni %23
+
+let letra = letra[nletra];
+
+if (letra == userletra){
+
+console.log("Las letras son iguales");
+console.log("La letra de tu DNI es:   "+ letra);
+
+}
+else{
+console.log("La letra ingresada es incorrecta");
+}
+
+console.log("La letra de tu DNI es: "+ letra);
 
 
 
 }
-
+else{
+console.log("Es incorrecto")
+}
+}
 
