a41dc6dcfe3a30e6915252fb70fcc11e52f78226
JorgeFuentes4124 <jorgee2007fc@gmail.com>
2024-03-25 14:36:25 -0300

    Este es el js de los ejercicios

---
+++ index.js
@@ -0,0 +1,68 @@
+function ejercicio1() {
+  let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+ 
+  for (let i = 0; i < meses.length; i++) {
+ 
+    console.log(meses[i]);
+ 
+  }
+ 
+ }
+ 
+ function ejercicio2(){
+  
+  let valores = [true, 5, false, "hola", "adios",2];
+
+  if (valores[3].length < valores[4].length) {
+    console.log(valores[0]);
+    console.log("adios es mayor que hola");
+  
+  }
+else{
+  console.log(valores[2]);
+  console.log("no se cumple la condicion");
+
+
+}
+
+console.log(valores[1]+valores[5]);
+console.log(valores[1]-valores[5]);
+console.log(valores[1]/valores[5]);
+console.log(valores[1]*valores[5]);
+
+ }
+
+function ejercicio3(){
+
+var numero1 =5;
+var numero2 =8;
+
+if(numero1<numero2) {
+console.log("5 no es mayor que 8");
+
+}
+if(numero2>0){
+console.log("8 es positivo");
+
+}
+if(numero1!=0){
+console.log("5 es negativo o distinto a cero");
+
+}
+if(numero1++<numero2)
+console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que 8");
+
+
+
+
+}
+
+function ejercicio4(){
+
+
+
+
+
+}
+
+
